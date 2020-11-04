696229cdd5e2a3a6bd77995b814433570f04df7b
xiaoqing <xiaoqing.cheng@yitu-inc.com>
2020-11-04 19:56:10 +0800

    circleci

--- config/paths.js
+++ config/paths.js
@@ -53,7 +53,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('doc'),
+  appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
