62189784b271ce2fcf0846871eed98a7c276daa2
xiaoqing <xiaoqing.cheng@yitu-inc.com>
2020-11-24 15:31:09 +0800

    eslint 修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/interface-name-prefix': 0,
     '@typescript-eslint/no-var-requires': 1,
+    '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/triple-slash-reference': 0,
     'prettier/prettier': 0,
   },
